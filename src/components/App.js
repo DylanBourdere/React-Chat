1474a93fb80f387381c8760d976aa21db81dc4a1
loperiane <loperanes@gmail.com>
2017-05-11 00:38:20 +0200

    route connexion to chat OK

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
 			<div className="box">
 				<div>
 				<div className="messages">
-					<Message pseudo="antho"/>
+					<Message pseudo={this.props.params.pseudo}/>
 				</div>
 				<Formulaire />
 				</div>
--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
 class Connexion extends React.Component {
+
+	goToChat = event => {
+		event.preventDefault();
+		const pseudo = this.pseudoInput.value;
+		this.context.router.transitionTo(`/pseudo/${pseudo}`);
+	};
+
 	render() {
 		return (
-			<div className="connexionBox">
+			<div className="connexionBox" onSubmit={(e) => this.goToChat(e)} >
 			<form className="connexion">
-				<input type="text" placeholder="Pseudo" required />
+				<input type="text" placeholder="Pseudo" required ref={(input) => {this.pseudoInput = input}} />
 				<button type="submit">Go</button>
 			</form>
 			</div>
 		)
+	}
+
+	static contextTypes = {
+		router: React.PropTypes.object
 	}
 }
 
