da45cfef20277753cdaa3db2ad12469b2e914191
loperiane <loperanes@gmail.com>
2017-05-11 19:20:24 +0200

    Enregistrement des message et reset du formulaire

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,21 @@ import Formulaire from './Formulaire';
 import Message from './Message';
 
 class App extends React.Component {
+
+	state = {
+		messages: {}
+	}
+
+	addMessage = (message) => {
+		// Copier le state
+		const messages = {...this.state.messages};
+		// On ajoute le message avec une clé timestamp
+		const timestamp = Date.now();
+		messages[`message-${timestamp}`] = message;
+		// MAJ du state
+		this.setState({ messages });
+	};
+
 	render() {
 		return (
 			<div className="box">
@@ -10,7 +25,7 @@ class App extends React.Component {
 				<div className="messages">
 					<Message pseudo={this.props.params.pseudo}/>
 				</div>
-				<Formulaire />
+				<Formulaire addMessage={this.addMessage} />
 				</div>
 			</div>
 		)
--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
 class Formulaire extends React.Component {
+
+	createMessage = event => {
+		event.preventDefault();
+		console.log(this.message.value);
+		const message = this.message.value;
+
+		this.props.addMessage(message);
+
+		this.messageForm.reset();
+	};
+
 	render() {
 		return (
-			<form className="form">
-				<textarea required maxLength="140"></textarea>
+			<form 
+				className="form"
+				onSubmit={(e) => this.createMessage(e)}
+				ref={input => this.messageForm = input}
+			>
+				<textarea
+					required
+					maxLength="140"
+					ref={input => this.message = input}
+					>
+
+					</textarea>
 
 				<div className="info">140</div>
 
